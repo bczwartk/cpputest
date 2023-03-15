4cdc1a48060c83f7a9e7aaf81a330ecd001c38a4
Boguslaw Czwartkowski <boguslaw.czwartkowski@parasoft.com>
2023-03-15 23:29:59 +0100

    Implemented make-based coverage simple run

---
+++ examples/run_tests.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -x
+
+# assumes that CppUTest library was already built and is available in ../lib
+# see ../parasoft/build_cpputest.sh
+
+
+# common variables
+CPPTEST_PRO_HOME=${HOME}/parasoft/cpptest-pro-2022.2.0
+CPPTEST_STD_HOME=${HOME}/parasoft/cpptest-std-2022.2.0
+CPPTESTCC=${CPPTEST_STD_HOME}/bin/cpptestcc
+# CPPTESTCC=${CPPTEST_PRO_HOME}/bin/cpptestcc
+CPPTESTCC_WORKSPACE=`pwd`
+CPPTESTCC_FLAGS="-compiler gcc_9-64 -line-coverage -mcdc-coverage -decision-coverage -workspace ${CPPTESTCC_WORKSPACE}"
+
+
+# sanity check - this just builds and runs tests in verbose mode, no C/C++test
+# make clean all SILENCE= CPPUTEST_EXE_FLAGS=-v
+
+
+# prepare C/C++test runtime library for coverage if not available yet
+CPPTEST_RT_LIB=./cpptest-runtime/build/cpptest.o
+if [ ! -d ./cpptest-runtime ] ; then
+  cp -r ${CPPTEST_STD_HOME}/runtime ./cpptest-runtime
+  # cp -r ${CPPTEST_PRO_HOME}/bin/engine/coverage/runtime ./cpptest-runtime
+fi
+if [ ! -f ${CPPTEST_RT_LIB} ] ; then
+  pushd ./cpptest-runtime
+  make
+  popd
+fi
+ls -l ${CPPTEST_RT_LIB}
+
+
+# cleanup old instrumentation cache if needed
+rm -rf ${CPPTESTCC_WORKSPACE}/.cpptest
+# cleanup old coverage data log if needed (it normally appends across runs)
+rm -f ./cpptest_results.clog
+
+
+# instrument for coverage and run tests 
+# - prefix compiler with cpptestcc command line - this will instrument for coverage
+# - add C/C++test runtime library to linker command line to add coverage API implementation
+#   try LDFLAGS or CPPUTEST_ADDITIONAL_LDFLAGS, or "+= ${CPPTEST_RT_LIB} in the makefile
+# for this project setting LDFLAGS or CPPUTEST_ADDITIONAL_LDFLAGS simply works
+make clean all SILENCE= CPPUTEST_EXE_FLAGS=-v \
+  CC="${CPPTESTCC} ${CPPTESTCC_FLAGS} -- gcc" \
+  CXX="${CPPTESTCC} ${CPPTESTCC_FLAGS} -- g++" \
+  LDFLAGS=${CPPTEST_RT_LIB}
+
+
+# the result will be a new coverage cache and runtime coverage data from the tests (.clog file)
+ls -l ${CPPTESTCC_WORKSPACE}/.cpptest
+ls -l ./cpptest_results.clog
+ls -l ./cpptest_results.utlog
+
+
+# generate report with coverage and CppUTest results
+# note: this required C/C++test Standard
+
+# theory...
+if [ ! false ] ; then
+# generate reports
+# rm -rf ./reports
+$CPPTEST_STD_HOME/cpptestcli \
+    -showdetails \
+    -workspace ${CPPTESTCC_WORKSPACE} \
+    -input ./build_cpputest/cpptest-coverage/CppUTest/CppUTest.clog \
+    -input ./cpptest_results.utlog \
+    -input ./build_cpputest/examples/AllTests/cpptest_results.utlog \
+    -module ./cpputest \
+    -config "builtin://Unit Testing" \
+    -property dtp.project=CppUTest \
+    -report ./reports \
+    $*
+ls -lart ./reports
+fi
+
+# ... and practice
+# note: C/C++test Standard can only report line coverage
+rm -rf ./reports
+$CPPTEST_STD_HOME/cpptestcli \
+    -showdetails \
+    -workspace ${CPPTESTCC_WORKSPACE} \
+    -input ./cpptest_results.clog \
+    -module . \
+    -config "builtin://Unit Testing" \
+    -property dtp.project=CppUTest \
+    -report ./reports \
+    $*
+ls -lart ./reports
+
+
+
